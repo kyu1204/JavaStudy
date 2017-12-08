a340e4b3e69f7b74a7c276e8402695d085d42a74
USER <USER@DESKTOP-LDT4UEG>
2017-12-08 11:14:09 +0900

    2017_12_08 11:14

---
+++ JavaProject/src/Project/MenuTest.java
@@ -0,0 +1,13 @@
+package Project;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class MenuTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
